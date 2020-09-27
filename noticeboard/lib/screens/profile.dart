a7f443b03ec8ac853dd939babbd3a6962f9bbe67
Sparsh1212 <sparshagrawal1212@gmail.com>
2020-09-27 13:38:36 +0530

    Make Profile UI 1

--- noticeboard/lib/screens/profile.dart
+++ noticeboard/lib/screens/profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/user_profile.dart';
+import '../styles/profile_constants.dart';
 
 class Profile extends StatefulWidget {
   final UserProfile userProfile;
@@ -12,10 +13,112 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
+    final double _width = MediaQuery.of(context).size.width;
+    final double _height = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Center(
-        child: Text(widget.userProfile.fullName),
+        body: SingleChildScrollView(
+      child: Center(
+        child: Container(
+          margin: EdgeInsets.only(top: _height * 0.11),
+          width: _width * 0.84,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                radius: 45.0,
+                backgroundColor: Colors.grey[400],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                widget.userProfile.fullName,
+                style: blackSuperBoldMediumSize,
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                widget.userProfile.degreeName,
+                style: lightGreySmallSize,
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                widget.userProfile.currentYear,
+                style: lightGreySmallSize,
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Text(
+                widget.userProfile.branchName,
+                style: lightGreySmallSize,
+              ),
+              SizedBox(
+                height: 40.0,
+              ),
+              buildMenuItem(
+                  Icon(
+                    Icons.collections_bookmark,
+                    color: Colors.grey[600],
+                  ),
+                  'Bookmarks'),
+              SizedBox(
+                height: 17.0,
+              ),
+              Container(
+                color: Colors.grey[400],
+                width: _width,
+                height: 2.0,
+              ),
+              SizedBox(
+                height: 17.0,
+              ),
+              buildMenuItem(
+                  Icon(
+                    Icons.feedback,
+                    color: Colors.grey[600],
+                  ),
+                  'Feedback'),
+              SizedBox(
+                height: 12.0,
+              ),
+              buildMenuItem(
+                  Icon(
+                    Icons.settings,
+                    color: Colors.grey[600],
+                  ),
+                  'Notification settings'),
+              SizedBox(
+                height: 15.0,
+              ),
+              buildMenuItem(
+                  Icon(
+                    Icons.exit_to_app,
+                    color: Colors.grey[600],
+                  ),
+                  'Logout')
+            ],
+          ),
+        ),
       ),
+    ));
+  }
+
+  Row buildMenuItem(Icon icon, String text) {
+    return Row(
+      children: [
+        icon,
+        SizedBox(
+          width: 15.0,
+        ),
+        Text(
+          text,
+          style: boldMediumGreyMediumSize,
+        )
+      ],
     );
   }
 }
---
+++ noticeboard/lib/styles/profile_constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+TextStyle blackSuperBoldMediumSize =
+    TextStyle(fontWeight: FontWeight.bold, fontSize: 25.0);
+
+TextStyle lightGreySmallSize = TextStyle(color: Colors.grey[500]);
+
+TextStyle boldMediumGreyMediumSize = TextStyle(
+    fontWeight: FontWeight.w400, color: Colors.grey[600], fontSize: 15.0);
