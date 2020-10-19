e040576238f407b840c286078d9df0a5f1f4ce9e
Sparsh1212 <sparshagrawal1212@gmail.com>
2020-10-19 23:24:57 +0530

    Rectify image not displaing bug in notice detail

--- noticeboard/lib/models/notice_content.dart
+++ noticeboard/lib/models/notice_content.dart
@@ -2,7 +2,13 @@ class NoticeContent {
   final String content;
   NoticeContent({this.content});
 
+  static String htmlCorrector(String incorrectHTML) {
+    String correctHTML = incorrectHTML.replaceAll(
+        "src=\"/media/", "src=\"https://internet.channeli.in/media/");
+    return correctHTML;
+  }
+
   factory NoticeContent.fromJSON(dynamic json) {
-    return NoticeContent(content: json['content']);
+    return NoticeContent(content: htmlCorrector(json['content']));
   }
 }
